de3467859bfc3034a947d9a406ebedb0acb7caa6
Ayush Verma <av555v5@gmail.com>
2024-12-16 15:47:51 +0530

    added nav bar for mobile view and fixed text sizing

--- src/components/EventCardButton.tsx
+++ src/components/EventCardButton.tsx
@@ -2,12 +2,12 @@ import {Link} from "react-router-dom";
 
 const EventCardButton:React.FC<{ icon?: React.ReactNode; title: string; link:string }> = ({ icon, title, link  }) => {
     return (
-        <div className="group flex gap-4 items-center relative overflow-hidden px-8 py-3 rounded-full w-fit h-fit border-[#8585C6] border text-3xl after:absolute after:w-full after:h-full after:bg-[#a8a8d7] after:left-0 after:-bottom-full after:rounded-full hover:after:bottom-0 hover:after:rounded hover:after:transition-all hover:after:ease-out hover:after:duration-200">
-            <Link to={link} className=" no-underline relative z-10 font-Roobert group-hover:text-black group-hover:no-underline group-hover:cursor-pointer">
+        <Link to={link} className="group flex gap-4 items-center relative overflow-hidden px-8 py-3 rounded-full w-fit h-fit border-[#8585C6] border text-3xl after:absolute after:w-full after:h-full after:bg-[#a8a8d7] after:left-0 after:-bottom-full after:rounded-full hover:after:bottom-0 hover:after:rounded hover:after:transition-all hover:after:ease-out hover:after:duration-200">
+            <div className=" no-underline relative z-10 font-Roobert group-hover:text-black group-hover:no-underline group-hover:cursor-pointer">
                 {title}
-            </Link>
+            </div>
             {icon}
-        </div>
+        </Link>
     )
 }
 export default EventCardButton
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 import {useEffect, useRef, useState} from "react";
 import {useWindowScroll} from "react-use";
 import gsap from "gsap";
-import {IoMenu} from "react-icons/io5";
+import {IoCloseSharp, IoMenu} from "react-icons/io5";
 
 
 interface MenuItem {
@@ -16,6 +16,7 @@ function Navbar() {
     const {y: yScroll} = useWindowScroll();
     const [lastScrollPosition, setLastScrollPosition] = useState(0);
     const [isNavVisible, setNavVisible] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         if (!navRef.current) return;
@@ -25,6 +26,7 @@ function Navbar() {
         } else if (yScroll > lastScrollPosition) {
             setNavVisible(false);
             navRef.current.classList.add("nav-bg");
+            setIsOpen(false)
         } else if (yScroll < lastScrollPosition) {
             setNavVisible(true);
             navRef.current.classList.add("nav-bg");
@@ -40,6 +42,25 @@ function Navbar() {
         });
     }, [isNavVisible]);
 
+    useEffect(() => {
+        if (!navRef.current) return;
+        if (isOpen) {
+            gsap.to(navRef.current, {
+                height: "100%",
+                duration: 0.7,
+            })
+            navRef.current.classList.add("nav-full");
+            navRef.current.classList.add("flex-col");
+        } else {
+            gsap.to(navRef.current, {
+                height: "80",
+                duration: 0.3,
+            })
+            navRef.current.classList.remove("nav-full");
+            navRef.current.classList.remove("flex-col");
+        }
+    }, [isOpen]);
+
     const menuItems: Array<MenuItem> = [
         {title: "Home", link: "/ahouba/"},
         {title: "Events", link: "/ahouba/events"},
@@ -48,20 +69,48 @@ function Navbar() {
     ]
 
     return (
-        <div ref={navRef} className="fixed top-0 left-0 z-30 w-full flex items-center justify-between align-middle px-7">
-            <img className="h-[70px] w-[70px] hover:scale-125 transition-all ease-in duration-300" src={Deer}
-                 alt="icon"/>
-            <IoMenu className="h-[35px] w-[40px] stroke-[#DFDFF2] mx-3 md:hidden"/>
-            <div className="hidden md:flex w-full items-center justify-end gap-6">
-                {
-                    menuItems.map(((menuItem, index) => (
-                        <div key={index} className="group relative overflow-hidden px-8 py-3 rounded-full border-[#DFDFF2] border text-3xl after:absolute after:w-full after:h-full after:bg-[#DFDFF2] after:left-0 after:-bottom-full after:rounded-full hover:after:bottom-0 hover:after:rounded hover:after:transition-all hover:after:ease-out hover:after:duration-200">
-                            <Link to={menuItem.link} className="w-full h-full no-underline relative z-10 text-[#DFDFF2] group-hover:text-[#A8A8D7] group-hover:no-underline group-hover:cursor-pointer">
-                                {menuItem.title}
+        <div ref={navRef}
+             className="fixed top-0 left-0 z-30 w-full flex items-center justify-between align-middle px-7">
+            <div className="w-full flex items-center justify-between">
+                <img className="h-[70px] w-[70px] hover:scale-125 transition-all ease-in duration-300" src={Deer}
+                     alt="icon"/>
+                <div className="h-[35px] w-[40px] relative mx-3 md:hidden">
+                    {!isOpen ?
+                        <IoMenu onClick={() => setIsOpen((open) => !open)}
+                                className="stroke-[#DFDFF2] h-full w-full"/> :
+                        <IoCloseSharp onClick={() => setIsOpen((open) => !open)}
+                                      className="fill-[#DFDFF2] h-full w-full"/>
+                    }
+                </div>
+            </div>
+            {isOpen && (
+                <div className="w-full h-full flex flex-col gap-10">
+                    {
+                        menuItems.map(((menuItem, index) => (
+                            <Link to={menuItem.link} key={index} onClick={() => setIsOpen((open) => !open)}
+                                  className="group relative overflow-hidden px-8 py-3 text-3xl active:bg-[#DFDFF2] rounded-full after:absolute after:w-full after:h-full after:bg-[#DFDFF2] after:left-0 after:-bottom-full after:rounded-full hover:after:bottom-0 hover:after:transition-all hover:after:ease-out hover:after:duration-200">
+                                <div
+                                    className="w-fit h-full no-underline relative z-10 text-[#DFDFF2] group-hover:text-[#A8A8D7] group-hover:no-underline group-hover:cursor-pointer">
+                                    {menuItem.title}
+                                </div>
                             </Link>
-                        </div>
+                        )))
+                    }
+                </div>
+            )}
+            <div className="hidden md:flex w-full items-center justify-end gap-6">
+                {!isOpen && (
+
+                    menuItems.map(((menuItem, index) => (
+                        <Link to={menuItem.link} key={index}
+                              className="group relative overflow-hidden px-8 py-3 rounded-full border-[#DFDFF2] border text-3xl after:absolute after:w-full after:h-full after:bg-[#DFDFF2] after:left-0 after:-bottom-full after:rounded-full hover:after:bottom-0 hover:after:rounded hover:after:transition-all hover:after:ease-out hover:after:duration-200">
+                            <div
+                                className="w-full h-full no-underline relative z-10 text-[#DFDFF2] group-hover:text-[#A8A8D7] group-hover:no-underline group-hover:cursor-pointer">
+                                {menuItem.title}
+                            </div>
+                        </Link>
                     )))
-                }
+                )}
             </div>
         </div>
     )
